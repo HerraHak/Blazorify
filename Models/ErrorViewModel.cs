aaf33022c13393dcbd29142659570334d260fd54
Hakim BEN AMAR <hakim@hf-online.be>
2019-06-10 20:33:19 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Blazorify.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
